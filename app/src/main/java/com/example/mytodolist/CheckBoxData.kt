d216764eaa08ed88d98a4ca74e923886eb01e355
sonms <71806591+sonms@users.noreply.github.com>
2022-10-12 15:17:01 +0900

    Update and rename app/src/main/java/com/example/mytodolist/CheckBoxData.kt to 앱/src/기본/자바/com/예시/신화주의자/CheckBoxData.kt

--- app/src/main/java/com/example/mytodolist/CheckBoxData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mytodolist
-
-data class CheckBoxData(
-    var id : Int, //position값
-    var checked : Boolean //check
-)
---
+++ 앱/src/기본/자바/com/예시/신화주의자/CheckBoxData.kt
@@ -0,0 +1,6 @@
+package com.example.mytodolist
+
+data class CheckBoxData(
+    var id : Int, //현재 아이템의 position값
+    var checked : Boolean //check 되었는지 여부
+)
